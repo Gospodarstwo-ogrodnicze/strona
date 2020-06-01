40d1e6e540303badda0577add0df5b338af80a79
Gospodarstwo-ogrodnicze <65110123+Gospodarstwo-ogrodnicze@users.noreply.github.com>
2020-06-01 13:29:10 +0200

    Add files via upload

---
+++ aktualnosci/wielkanocnezyczenia2019-20.php
@@ -0,0 +1,17 @@
+<?php header('Content-Type: text/html; charset=UTF-8');?>
+<div class="newsTytul">
+Życzenia Świąteczne
+</div>
+<div class="newsTresc">
+"Wielkanoc to czas otuchy i nadziei, to czas odradzania wszystkiego, co piękne i dobre w człowieku"...<br>
+
+<a href="aktualnosci/2019-20/wielkanocnezyczenia19-20.pdf" target="_blank">Przeczytaj więcej </a>&nbsp;&nbsp;
+
+   </div>
+
+<div class="newsAutor">
+Administrator
+</div>
+<div class="newsData">
+2020.04.09
+</div>
